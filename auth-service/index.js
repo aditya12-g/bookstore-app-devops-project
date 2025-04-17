4395fd31ee290d1d50000b77dbed0449d3d3abf0
Aditya Gangthade <adityagangthade1998@gmail.com>
2025-04-17 13:09:39 +0530

    Update index.js

--- auth-service/index.js
+++ auth-service/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+// Define a GET route for the root path
+app.get('/', (req, res) => {
+    res.send('Welcome to the Auth Service!');
+});
+
 app.post('/signup', (req, res) => {
     res.json({ message: 'User signed up' });
 });
