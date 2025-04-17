fab60cddeef4c2088fda4c3bea788430876a004a
Aditya Gangthade <adityagangthade1998@gmail.com>
2025-04-17 13:05:15 +0530

    Update index.js

--- book-service/index.js
+++ book-service/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+// Define a route for the root path
+app.get('/', (req, res) => {
+    res.send('Welcome to the Book Service!');
+});
+
+// Define a route for the /books path
 app.get('/books', (req, res) => {
     res.json([{ id: 1, title: 'The Great Gatsby' }]);
 });
 
 app.listen(3002, () => console.log('Book Service on port 3002'));
+
