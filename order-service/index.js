afd8f4571009e382f66604c7ffea1ed465dd3bfb
Aditya Gangthade <adityagangthade1998@gmail.com>
2025-04-17 13:07:31 +0530

    Update index.js

--- order-service/index.js
+++ order-service/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+// Define a GET route for the root path
+app.get('/', (req, res) => {
+    res.send('Welcome to the Order Service!');
+});
+
 app.post('/order', (req, res) => {
     res.json({ message: 'Order placed' });
 });
